27da20af7975c722531a479b013e16b9c66a46a0
singhsanket143 <singhsanket143@gmail.com>
2019-05-21 18:38:18 +0530

    Handled Form Submission

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
       </div>
     );
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,8 +1,29 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
-    return <div>Search Bar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <label className="field">Search Videos</label>
+          <input
+            onChange={this.onInputChange}
+            type="text"
+            value={this.state.term}
+          />
+        </form>
+      </div>
+    );
   }
 }
 
